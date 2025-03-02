e7e35db31f6566b1ef5fc7b233207baf9f315860
takumines <takumines1@gmail.com>
2025-03-02 17:00:02 +0900

    [update] DB接続の設定変更

--- src/fuel/app/config/development/db.php
+++ src/fuel/app/config/development/db.php
@@ -22,9 +22,9 @@
 return array(
 	'default' => array(
 		'connection' => array(
-			'dsn'      => 'mysql:host=localhost;dbname=fuel_dev',
-			'username' => 'root',
-			'password' => 'root',
+			'dsn'      => 'mysql:host=db;dbname=fuel_db',
+			'username' => 'phper',
+			'password' => 'secret',
 		),
 	),
 );
