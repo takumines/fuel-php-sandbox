314004e41fef6a5774343bad6a319b4efa7ad702
takumines <takumines1@gmail.com>
2025-04-16 23:44:47 +0900

    [update] ヘッダーメニューを追加

--- src/fuel/app/views/template.php
+++ src/fuel/app/views/template.php
@@ -8,8 +8,24 @@
 		body { margin: 40px; }
 	</style>
 </head>
+
 <body>
+    <nav class="navbar navbar-default">
+        <div class="container-fluid">
+            <div class="collapse navbar-collapse">
+                <ul class="nav navbar-nav navbar-right">
+                    <?php if (\Auth::check()): ?>
+                        <li><a href="<?php echo Uri::create('auth/logout'); ?>">ログアウト</a></li>
+                    <?php else: ?>
+                        <li><a href="<?php echo Uri::create('auth/login'); ?>">ログイン</a></li>
+                        <li><a href="<?php echo Uri::create('auth/register'); ?>">新規作成</a></li>
+                    <?php endif; ?>
+                </ul>
+            </div>
+        </div>
+    </nav>
 	<div class="container">
+
 		<div class="col-md-12">
 			<h1><?php echo $title; ?></h1>
 			<hr>
