baa72c5e93a756a2b927ad3bac78957b7f284839
takumines <takumines1@gmail.com>
2025-04-17 16:39:55 +0900

    [add] 記事一覧・詳細画面のviewを作成

--- src/fuel/app/views/article/index.php
+++ src/fuel/app/views/article/index.php
@@ -1,11 +1,9 @@
-<h2><span class='muted'>記事一覧</span></h2>
-<br>
 <?php if ($articles): ?>
     <table class="table table-striped">
         <thead>
         <tr>
-            <th>Title</th>
-            <th>Content</th>
+            <th>タイトル</th>
+            <th>内容</th>
             <th>&nbsp;</th>
         </tr>
         </thead>
@@ -13,13 +11,15 @@
         <?php foreach ($articles as $article): ?>		<tr>
 
             <td><?php echo $article->title; ?></td>
-            <td><?php echo $article->content; ?></td>
+            <td><?php echo $article->body; ?></td>
             <td>
                 <div class="btn-toolbar">
                     <div class="btn-group">
-                        <?php echo Html::anchor('articles/'.$article->id, '<i class="icon-eye-open"></i> View', array('class' => 'btn btn-default btn-sm')); ?>
-                        <?php echo Html::anchor('post/edit/'.$item->id, '<i class="icon-wrench"></i> Edit', array('class' => 'btn btn-default btn-sm')); ?>
-                        <?php echo Html::anchor('post/delete/'.$item->id, '<i class="icon-trash icon-white"></i> Delete', array('class' => 'btn btn-sm btn-danger', 'onclick' => "return confirm('Are you sure?')")); ?>
+                        <?php echo Html::anchor('articles/view/'.$article->id, '<i class="icon-eye-open"></i> 閲覧', array('class' => 'btn btn-default btn-sm')); ?>
+                        <?php if (Auth::get('id') == $article->user_id): ?>
+                            <?php echo Html::anchor('articles/edit/'.$article->id, '<i class="icon-wrench"></i> 編集', array('class' => 'btn btn-default btn-sm')); ?>
+                            <?php echo Html::anchor('articles/delete/'.$article->id, '<i class="icon-trash icon-white"></i> 削除', array('class' => 'btn btn-sm btn-danger', 'onclick' => "return confirm('本当に削除しますか？')")); ?>
+                        <?php endif; ?>
                     </div>
                 </div>
 
@@ -29,9 +29,9 @@
     </table>
 
 <?php else: ?>
-    <p>No Posts.</p>
+    <p>記事がありません。</p>
 
 <?php endif; ?><p>
-    <?php echo Html::anchor('articles/create', 'Add new Post', array('class' => 'btn btn-success')); ?>
+    <?php echo Html::anchor('articles/create', '新規作成', array('class' => 'btn btn-success')); ?>
 
 </p>
--- src/fuel/app/views/article/show.php
+++ src/fuel/app/views/article/show.php
@@ -1,11 +1,12 @@
-<h2>Viewing <span class='muted'>#<?php echo $article->id; ?></span></h2>
-
 <p>
     <strong>Title:</strong>
     <?php echo $article->title; ?></p>
 <p>
     <strong>Content:</strong>
-    <?php echo $article->content; ?></p>
+    <?php echo $article->body; ?></p>
 
-<?php echo Html::anchor('articles/edit/'.$article->id, 'Edit'); ?> |
-<?php echo Html::anchor('post', 'Back'); ?>
+<?php if (Auth::get('id') == $article->user_id): ?>
+    <?php echo Html::anchor('articles/edit/'.$article->id, 'Edit'); ?> |
+    <?php echo Html::anchor('articles/delete/'.$article->id, 'Delete', ['onclick' => "return confirm('Are you sure?')"]); ?>
+<?php endif; ?> |
+<?php echo Html::anchor('articles', 'Back'); ?>
